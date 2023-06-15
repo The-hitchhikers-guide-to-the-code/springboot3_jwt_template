323679fa1920aa72a13c8646b9987131ab4ff959
ygpark <ygpark@fassto.ai>
2023-06-15 15:23:38 +0900

    first commit

---
+++ src/main/java/io/pinkspider/springboot3jwttemplate/Springboot3JwtTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.pinkspider.springboot3jwttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JwtTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3JwtTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pinkspider/springboot3jwttemplate/Springboot3JwtTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pinkspider.springboot3jwttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3JwtTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
